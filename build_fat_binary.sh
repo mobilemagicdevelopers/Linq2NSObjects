ee51c8fc9a905951a51b5e893775dc1f5586ab39
James Brechtel <jbrechtel@gmail.com>
2013-03-31 17:00:52 -0400

    Adding script to build a fat binary with multiple architectures so this can be used in both simulator and devices

---
+++ build_fat_binary.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+xcodebuild -sdk iphoneos6.1 "ARCHS=armv6 armv7" build > /dev/null
+xcodebuild -sdk iphonesimulator6.1 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" build > /dev/null
+
+rm -rf fat_binary
+mkdir fat_binary
+
+lipo -output fat_binary/libLinq2NSObjects.a -create build/Release-iphoneos/libLinq2NSObjects.a build/Release-iphonesimulator/libLinq2NSObjects.a
+
+cp -R build/Release-iphoneos/include fat_binary
