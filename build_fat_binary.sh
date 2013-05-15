e350e6448c7484d612f46f4b2e5e06ec37d645cc
Patrick Caldwell <patrick@mobilemagicdevelopers.com>
2013-05-14 20:41:11 -0400

    Added armv7s.

--- build_fat_binary.sh
+++ build_fat_binary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xcodebuild -sdk iphoneos6.1 "ARCHS=armv6 armv7" build > /dev/null
+xcodebuild -sdk iphoneos6.1 "ARCHS=armv6 armv7 armv7s" build > /dev/null
 xcodebuild -sdk iphonesimulator6.1 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" build > /dev/null
 
 rm -rf fat_binary
