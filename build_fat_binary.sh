f484b74dfe0aaea7e8f020cdb4c1672fe43b4455
Patrick Caldwell <tncbbthositg@gmail.com>
2015-02-06 01:13:55 -0500

    patrick | updated to 8.1 and added arm64

--- build_fat_binary.sh
+++ build_fat_binary.sh
@@ -7,8 +7,8 @@ rm -rf build
 mkdir build
 
 echo "Building $TARGET."
-xcodebuild -target "$TARGET" -sdk iphoneos6.1 "ARCHS=armv6 armv7 armv7s" -configuration "Release" clean build > /dev/null
-xcodebuild -target "$TARGET" -sdk iphonesimulator6.1 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" -configuration "Release" clean build > /dev/null
+xcodebuild -target "$TARGET" -sdk iphoneos8.1 "ARCHS=armv6 armv7 armv7s arm64" -configuration "Release" clean build > /dev/null
+xcodebuild -target "$TARGET" -sdk iphonesimulator8.1 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" -configuration "Release" clean build > /dev/null
 
 echo "Clean up output folder."
 rm -rf fat_binary
